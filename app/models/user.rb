6e6201198db9f5f7f81a87cd0e2e01afc741322a
Tristen Unrau <tristen@unrau.me>
2016-07-16 15:20:05 -0700

    Fix syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                     length: { maximum: 50 }
   validates :email, presence: true,
                     length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true,
