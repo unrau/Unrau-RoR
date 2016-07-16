cf8c2b8f2487585baab4e990c9a04846f8aef376
Tristen Unrau <tristen@unrau.me>
2016-07-16 14:23:16 -0700

    Refactor html titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def title(page_title, hide_base=false)
+    base_title = 'Website Title'
+    if page_title.length > 0
+      if hide_base
+        base_title = page_title.to_s
+      else
+        base_title = base_title + ' - ' + page_title.to_s
+      end
+    end
+    provide :title, base_title
+  end
+
 end
