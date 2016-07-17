922be93228b3869f5e388ded85ede3d8d5853301
Tristen Unrau <tristen@unrau.me>
2016-07-17 11:08:16 -0700

    Add basic login/logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
       flash[:success] = "Account successfully created."
+      redirect_to @user
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/signup', to: 'users#new'
+  get '/signup',    to: 'users#new'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  get '/logout',    to: 'sessions#destroy'
 
 end
