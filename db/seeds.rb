fbe6f9120f90198a9df7a507a5f2360f1eeb72e7
Tristen Unrau <tristen@unrau.me>
2016-07-17 12:57:50 -0700

    Initialize a seeded admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:                  "ADMIN",
+             email:                 "admin@example.com",
+             password:              "adminadmin",
+             password_confirmation: "adminadmin")
