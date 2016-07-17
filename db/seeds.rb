d6daef8db144b107921416cada3567c9c5d95046
Tristen Unrau <tristen@unrau.me>
2016-07-17 13:35:34 -0700

    Paginate the list of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,15 @@ User.create!(name:                  "ADMIN",
              email:                 "admin@example.com",
              password:              "adminadmin",
              password_confirmation: "adminadmin")
+
+if Rails.env == "development"
+  99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@example.com"
+    password = "password"
+    User.create!(name: name,
+                 email: email,
+                 password: password,
+                 password_confirmation: password)
+  end
+end
