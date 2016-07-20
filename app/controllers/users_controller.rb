880cfda351386e86ce23b4c6b8c4fec5a9b86b09
Tristen Unrau <tristen@unrau.me>
2016-07-20 14:12:54 -0700

    Allow non-logged-in users to see user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :new, :create, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
