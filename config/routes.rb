bffeb04916254582a9c15086eafb9ed70fcb92eb
Unrau <tristen@unrau.me>
2016-09-28 19:14:20 -0700

    Create YouTime page
    Allows creation of YouTube links that begin at a specified time

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,16 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/signup',      to: 'users#new'
+  get 'signup',      to: 'users#new'
 
-  get '/login',       to: 'sessions#new'
-  post '/login',      to: 'sessions#create'
-  get '/logout',      to: 'sessions#destroy'
+  get 'login',       to: 'sessions#new'
+  post 'login',      to: 'sessions#create'
+  get 'logout',      to: 'sessions#destroy'
 
-  get '/wallpapers',  to: 'static_pages#wallpapers'
-  get '/fiasco',      to: 'static_pages#fiasco'
+  get 'wallpapers',  to: 'static_pages#wallpapers'
+  get 'fiasco',      to: 'static_pages#fiasco'
+
+  get 'youtime',     to: 'youtime#index'
+  post 'youtime',    to: 'youtime#generate'
 
 end
