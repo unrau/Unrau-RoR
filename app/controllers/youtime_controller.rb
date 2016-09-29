936c3ef4043d663bf02e86ac582322a9b0839c43
Unrau <tristen@unrau.me>
2016-09-28 23:29:58 -0700

    Construct only the necessary url elements for each case

--- app/controllers/youtime_controller.rb
+++ app/controllers/youtime_controller.rb
@@ -26,7 +26,20 @@ class YoutimeController < ApplicationController
   private
 
     def generate_youtime(youtube_id, hour, minute, second)
-      return 'https://www.youtube.com/watch?v=' + youtube_id + '&t=' + hour + 'h' + minute + 'm' + second + 's'
+      newyou = 'https://www.youtube.com/watch?v=' + youtube_id
+      if hour.to_i > 0 or minute.to_i > 0 or second.to_i > 0
+        newyou += '&t='
+      end
+      if hour.to_i > 0
+        newyou += hour + 'h'
+      end
+      if minute.to_i > 0
+        newyou += minute + 'm'
+      end
+      if second.to_i > 0
+        newyou += second + 's'
+      end
+      return newyou
     end
 
 end
